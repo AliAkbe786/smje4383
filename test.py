b89333b2d33610d9fa44c62b4218cf4eb8fcc559
AliAkbe786 <aliakbermm@gmail.com>
2024-11-08 06:13:16 +0800

    New test file

---
+++ test.py
@@ -0,0 +1,48 @@
+''' Introduction to python programming '''
+
+print ("Hello Smje4383")
+
+a = 2.00
+
+a = int(a)
+
+if (a == 2):
+    print("True")
+    print("The a value is:", a)
+    print(type(a))
+
+else:
+    print("False")
+
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
+#python string
+
+b = "Hello, World!"
+
+#calling function
+b = b.upper()
+
+print(b)
+
+i = 10
+
+while i < 20:
+    
+    i = i + 1
+    print("Increment i value",i)
+
+def displaytambah():
+    print("Just to display result")
+
+def tambah(a,b)
+    
+    c = a + b
+
+    print("The added result is: ",c)
+
+displaytambah()
+tambah(8.6,100)
